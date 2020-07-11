14d797adc0f0dc47082771d4eaa95aeffaeeec34
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-07-11 09:53:56 -0500

    Improved welcome message

--- eventActions/welcomeActions.js
+++ eventActions/welcomeActions.js
@@ -1,4 +1,5 @@
 const config = require('../config.json');
+const Discord = require('discord.js');
 
 class tosActions {
   static userAcceptsTOS(reaction, user, client) {
@@ -11,25 +12,38 @@ class tosActions {
         client.channels
           .get(config.channels.welcome)
           .send(
-            `🎉 **A new member has arrived!** 🎉\nPlease welcome <@${user.id}> to The Bookery <@&693517619457818634>!`
+            `🎉 **A new member has arrived!** 🎉\nPlease welcome <@${user.id}> to The Bookery <@&693517619457818634>!\n\`\`\`\`\`\``
           )
           .then((message) => {
             message.react(config.emotes.wave);
           });
-        user.send(`\`\`\`fix
-Welcome to The Bookery!\`\`\`
-To begin, say hello in <#693498873083330654> and follow our Quick Start Guide below.
 
-**Quick Start Guide**
-• You can grab some roles in <#693563108077076490>.
+        let welcomeDM = new Discord.RichEmbed()
+          .setColor('#750384')
+          .setTitle(':books: __**Welcome to The Bookery!**__ :books:')
+          .addField(
+            'Quick Start Guide',
+            `• You can grab some roles in <#693563108077076490>.
 • Introduce yourself with the template pinned in <#711269048591056916>.
-• To learn more about the server, check out <#693500724704837653>.
-
-**Reading Groups**
-We are a reading server, and we host multiple reading groups! Learn more in the info channel, and join in the self roles channel! Have fun!
-
-**About Me**
-I'm Finriq, a custom bot made for The Bookery! I am always growing. Use \`.help\` in <#693561975887888444> for more on what I can do.`);
+• To learn more about the server, check out <#693500724704837653>`
+          )
+          .addField(
+            'Reading Groups',
+            `We are a reading server, and we host multiple reading groups! Learn more in the info channel, and join in the self roles channel! Have fun!`
+          )
+          .addField(
+            'About The Bookery',
+            `The Bookery is a community dedicated to the creation, consumption, admiration, and discussion of literature. We strive to create an environment where readers can find a community of people who have their same interests and talk or read together.`
+          )
+          .addField(
+            'Safe Space',
+            `The Bookery is a safe space. We do not allow prejudice or hatred for anyone, explicit discussions, or offensive speech. We understand that some books may contain content that is more adult or offensive, and we ask you refrain from discussing them here.`
+          )
+          .addField(
+            'About Me',
+            `I'm Finriq, a custom bot made for The Bookery! I am always growing. Use \`.help\` in <#693561975887888444> for more on what I can do.`
+          );
+        user.send(welcomeDM);
       });
     }
   }
