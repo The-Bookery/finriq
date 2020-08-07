84d24c76ee6c1302e172f896335650714570ffbe
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-08-07 12:26:50 -0500

    Fixed error

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,13 +1,9 @@
-const tosActions = require('../eventActions/welcomeActions');
 const bookmarkActions = require('../eventActions/bookmarkActions');
 const accountabilityActions = require('../eventActions/accountabilityActions');
 
 module.exports = async (client, reaction, user) => {
   // Handle reaction to a message in accountability channel
   accountabilityActions.userPinsMessage(reaction, user);
-
-  // Handle reaction to the ToS message in ToS channel
-  tosActions.userAcceptsTOS(reaction, user, client);
 
   // Bookmark messages in DMs
   bookmarkActions.bookmarkMessage(client, user, reaction);
