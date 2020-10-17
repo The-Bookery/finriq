08e930c07b54fa52ab2a6dfa5e895a0581c78f9f
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-10-17 14:01:15 -0500

    Reverted revert of changes

--- events/message.js
+++ events/message.js
@@ -4,7 +4,6 @@ const reactions = require('../eventActions/reactions');
 const backspeak = require('../eventActions/backspeak');
 const cafeActions = require('../eventActions/cafeActions');
 const oneWordStory = require('../eventActions/oneWordStory');
-const profanityActions = require('../eventActions/profanityActions');
 
 module.exports = async (client, message) => {
   if (!message.guild || message.author.bot) return;
@@ -23,7 +22,6 @@ module.exports = async (client, message) => {
     }
   }
 
-	profanityActions.checkForProfanity(client, message);
   // Handle greetings
 	cafeActions.greetMorningOrNight(client, message);
   reactions.checkIfCorrect(message);
