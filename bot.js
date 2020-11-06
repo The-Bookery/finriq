6962a91855d113180102bf3fe19a97f33ec54933
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-11-06 15:56:50 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 const connect = require('./databaseFiles/connect.js');
 
-const client = new Discord.Client({ disableMentions: 'everyone', ws: { intents: 'GUILDS', 'GUILD_MEMBERS' }});
+const client = new Discord.Client({ disableMentions: 'everyone', ws: { intents: ['GUILDS', 'GUILD_MEMBERS'] }});
 
 fs.readdir('./events/', (err, files) => {
   if (err) return console.error(err);
