30262a3de5c34da9a20b1bfe1a77c35d188ad5cd
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-12-23 16:09:50 -0600

    Hi

--- eventActions/starboardActions.js
+++ eventActions/starboardActions.js
@@ -1,7 +1,6 @@
 class starboardActions {
   static async checkStar(user, reaction) {
-    console.log(reaction._emoji);
-		if (reaction._emoji && reaction._emoji.id === config.emotes.star) {
+		if (reaction._emoji && reaction._emoji.name === config.emotes.star) {
       var stars = reaction.count;
       var username = user.username;
       var avatar = user.avatar;
--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -2,6 +2,7 @@ const bookmarkActions = require('../eventActions/bookmarkActions');
 const starboardActions = require('../eventActions/starboardActions');
 
 module.exports = async (client, reaction, user) => {
+  console.log("first check");
   // Bookmark messages in DMs
   bookmarkActions.bookmarkMessage(client, user, reaction);
   // Check if message should be added to starboard or if starboard message should be updated
