440baea459d3b99e411f0e715645a63d5f1fa986
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-12-23 13:53:27 -0600

    More testing

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,14 @@
 const config = require('../config.json');
 
 module.exports = (client) => {
+	client.api.applications(client.user.id).guilds('791366170611679272').commands.post({
+		data: {
+				name: "hello",
+				description: "hello world command"
+				// possible options here e.g. options: [{...}]
+		}
+	});
+
 	console.log(`Running on ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers.`);
 	client.user.setActivity(config.playing);
 };
