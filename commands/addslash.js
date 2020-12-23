b8ac53df29303c1328d6f56e83b6ab5b8da44267
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-12-23 14:43:06 -0600

    Added AFK to slash

---
+++ commands/addslash.js
@@ -0,0 +1,23 @@
+module.exports.execute = async (client, message) => {
+  client.api.applications(client.user.id).guilds('guild id').commands.post({data: {
+    "name": 'afk',
+    "description": 'Lets other members know you\'ve gone away when they ping you.',
+    "options": [
+      {
+        "name": "message",
+        "description": "Will notify the person pinging you where you've gone.",
+        "type": 3,
+        "required": false
+      }
+  ]
+  }});
+};
+
+module.exports.config = {
+  name: 'addslash',
+  aliases: [],
+  module: 'Utility',
+  description:
+    'Adds slashes for testing.',
+  usage: ['addslash'],
+};
