851671f4c2876d7a3926af6a7730d9c929e951d5
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-12-23 15:22:17 -0600

    Removed slash commands, fixed welcome message

--- bot.js
+++ bot.js
@@ -39,16 +39,6 @@ fs.readdir('./commands/', (err, files) => {
   });
 });
 
-client.ws.on('INTERACTION_CREATE', async interaction => {
-  const command = interaction.data.name.toLowerCase();
-  const args = interaction.data.options;
-
-  console.log(command);
-  console.log(args);
-
-  client.emit('message', command + " " + args);
-});
-
 // Connect to given database
 connect.instantiateConnection();
 
