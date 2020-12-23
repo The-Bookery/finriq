f8e8f357a51b8848cdea98281da217e3344a6ace
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-12-23 13:55:07 -0600

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 const config = require('../config.json');
 
 module.exports = (client) => {
-	client.api.applications(client.user.id).guilds('791366170611679272').commands.post({
+	client.api.applications(client.user.id).guilds(791366170611679272).commands.post({
 		data: {
 				name: "hello",
 				description: "hello world command"
