690dbddbe66a68e41b0ba76f015ed8aeb2df2ec4
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-12-24 15:18:17 -0600

    Added delete message logs

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,5 +1,18 @@
 const starboardActions = require('../eventActions/starboardActions');
+const Discord = require('discord.js');
+const config = require('../config.json');
 
 module.exports = async (client, message, channel) => {
+  const embed = new Discord.MessageEmbed()
+    .setAuthor(message.author.username, message.author.displayAvatarURL())
+    .setTitle(`${message.author.username} deleted a message`)
+    .setDescription(`Message deleted in <#${channel.id}>.`)
+    .addField(
+      'Content',
+      message.content
+    )
+    .setColor('#ffb980');
+  client.channels.cache.get(config.channels.logs).send(embed);
+
   starboardActions.removeMessage(client, message);
 };
