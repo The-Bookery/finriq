3a7f7ad27e0886baee906106c0076d9a022de82b
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-12-23 21:36:26 -0600

    Readded intent

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const connect = require('./databaseFiles/connect.js');
 const client = new Discord.Client({
   disableMentions: 'everyone',
   ws: {
-    intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS']
+    intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS', 'GUILD_MESSAGE_REACTIONS']
   },
   partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
