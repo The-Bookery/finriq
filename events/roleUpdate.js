caafc39dbc41e0ed8e8561cf95418768ff69255b
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-12-25 14:49:30 -0600

    Fixed role update logs

--- events/roleUpdate.js
+++ events/roleUpdate.js
@@ -2,8 +2,6 @@ const config = require('../config.json');
 const Discord = require('discord.js');
 
 module.exports = async (client, oldRole, newRole) => {
-  console.log(oldRole);
-  /*
   const oldperms = oldRole.permissions.toArray().join("\n");
   const newperms = newRole.permissions.toArray().join("\n");
 
@@ -13,15 +11,24 @@ module.exports = async (client, oldRole, newRole) => {
   const oldcolor = oldRole.color;
   const newcolor = newRole.color;
 
+  const oldhoisted = oldRole.hoisted;
+  const newhoisted = newRole.hoisted;
+
+  const oldmention = oldRole.mentionable;
+  const newmention = oldRole.mentionable;
+
+  if (oldname === newname && oldperms === newperms && oldcolor === newcolor && oldhoisted === newhoisted && oldmention === newmention) return;
+
   const embed = new Discord.MessageEmbed()
     .setTitle(`Role Updated`)
     .setDescription(`The role \`${oldname}\` has been updated.`)
     .setColor('#ffb980');
 
-  if (oldname != newname) embed.addField('New Name', newname);
-  if (oldcolor != newcolor) embed.addField('New Color', newcolor);
-  if (oldperms != newperms) embed.addField('New Permissions', newperms);
+  if (oldname !== newname) embed.addField('New Name', newname);
+  if (oldcolor !== newcolor) embed.addField('New Color', newcolor);
+  if (oldperms !== newperms) embed.addField('New Permissions', newperms);
+  if (oldhoisted !== newhoisted) embed.addField('Hoisted', newhoisted);
+  if (oldmention !== newmention) embed.addField('Mentionable', newmention);
 
   client.channels.cache.get(config.channels.logs).send(embed);
-  */
 };
