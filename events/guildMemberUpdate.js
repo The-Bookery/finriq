eaea14946b4bf9e25d44dde0a8ed49dfa3a40fa8
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-12-31 14:40:26 -0600

    Add necessary dependencies

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,3 +1,6 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+
 module.exports = async (client, oldMember, newMember) => {
   if (oldMember.pending && !newMember.pending) {
     const memberrole = newMember.guild.roles.cache.find(role => role.id === '719660584806776884');
