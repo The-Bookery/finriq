5cfa05bfda2187162bc9833cd5bdae2dc8052652
CivilSports <78701348+CivilSports@users.noreply.github.com>
2021-03-16 10:06:13 -0500

    Updates to logs

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,8 @@ const config = require('../config.json');
 const Discord = require('discord.js');
 
 module.exports = async (client, member) => {
+  if(member.user.bot) return;
+  
   const embed = new Discord.MessageEmbed()
     .setAuthor(`${member.user.username}#${member.user.discriminator}`, member.user.displayAvatarURL())
     .setTitle(`Member Joined`)
