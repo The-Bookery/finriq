84202dbb44d784d7ab7f91096f7a501ab5514b6d
zmontgo <31627113+zmontgo@users.noreply.github.com>
2021-09-25 17:17:51 -0500

    Fix random errors

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -5,7 +5,7 @@ import { reportActions } from "../eventActions/reportAction";
 export = async (client, reaction, user) => {
   try {
     // When we receive a reaction we check if the reaction is partial or not
-    if (reaction.partial) {
+    if (reaction && reaction.partial) {
       // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
       reaction = await reaction.fetch();
     }
--- src/events/messageReactionRemove.ts
+++ src/events/messageReactionRemove.ts
@@ -2,7 +2,7 @@ import { starboardActions } from "../eventActions/starboardActions";
 
 export = async (client, reaction, user) => {
   // When we receive a reaction we check if the reaction is partial or not
-  if (reaction.partial) {
+  if (reaction && reaction.partial) {
     // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
     try {
       await reaction.fetch();
